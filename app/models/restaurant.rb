90e4e3dbd7c01503be03808c8d8fad4cb108a235
alishahal <alishahal@msn.com>
2020-03-26 11:02:48 -0500

    Module1

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,5 +4,27 @@ class Restaurant
   def initialize(name)
     @name = name
   end
+  
+  def name
+     @name
+  end
+
+  def reviews
+    Review.all.select do |r|
+      r.restaurant== self
+    end
+  end
+
+  def customers 
+    reviews.map do |r|
+      r.restaurant.uniq()
+    end
+  end
+
+  # def average_star_rating
+  #    avg=0
+  #    reviews.
+  # end
+
 
 end
